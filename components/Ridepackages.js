6636a92ef9a29a4aac60e8ab6483b97146350140
Deeksha Shetty <shettydeeksha810@gmail.com>
2025-04-11 11:54:43 +0400

    Update Ridepackages.js

--- components/Ridepackages.js
+++ components/Ridepackages.js
@@ -12,7 +12,7 @@ const Packages = () => {
 
   return (
     <div className="p-6 bg-gray-800 overflow-hidden rounded-lg ">
-      <h2 className="text-2xl font-bold mb-6 text-gray-100 ">Ride Packages</h2>
+      <h2 className="text-2xl font-bold mb-6 text-gray-100 "> Ride Package</h2>
       <div className="grid md:grid-cols-3 gap-6 ">
         {packages.map((pkg, index) => (
           <motion.div
