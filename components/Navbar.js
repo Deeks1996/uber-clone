01737011e963eef62308ee82255d33264a18184d
Deeksha Shetty <shettydeeksha810@gmail.com>
2025-04-11 12:58:56 +0400

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,13 @@
-import { useEffect,useState } from "react";
+"use client";
+import { useEffect, useState } from "react";
 import { UserButton, useUser } from "@clerk/nextjs";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
-import { FaCar } from "react-icons/fa6";
+import { FaCar, FaUser, FaHistory, FaPhone } from "react-icons/fa";
 import { PiPackageFill } from "react-icons/pi";
-import { FaUser } from "react-icons/fa";
-import { FaHistory } from "react-icons/fa";
 import { RiFeedbackFill } from "react-icons/ri";
 import { GrTransaction } from "react-icons/gr";
-import { FaPhone } from "react-icons/fa";
 
 const Navbar = ({ requestedCount }) => {
   const pathname = usePathname();
@@ -22,36 +20,32 @@ const Navbar = ({ requestedCount }) => {
     <nav className="bg-blue-900 text-white p-4 flex justify-between items-center">
       <h2 className="text-xl font-bold">Uber Clone</h2>
       <div className="flex flex-row gap-10 relative">
-       
         {role === "driver" ? (
-            <>
+          <>
             <Link
-            href="/driver-dashboard"
-            className={clsx(
-              "relative inline-flex items-center gap-1 px-2 py-1 rounded ",
-              isActive("/driver-dashboard") ? "text-white bg-black" : "text-white"
-            )}
-          >
-            <div className="flex items-center gap-1 relative">
+              href="/driver-dashboard"
+              className={clsx(
+                "relative inline-flex items-center gap-1 px-2 py-1 rounded",
+                isActive("/driver-dashboard") && "bg-black"
+              )}
+            >
               <FaCar />
               <span>Requests</span>
-          
               {requestedCount > 0 && (
-                <span className="absolute -top-2 -right-3 bg-red-600 text-white text-xs rounded-full px-1  shadow">
+                <span className="absolute -top-2 -right-3 bg-red-600 text-white text-xs rounded-full px-1 shadow">
                   {requestedCount}
                 </span>
               )}
-            </div>
-          </Link>
-          
-          <Link
+            </Link>
+
+            <Link
               href="/driver-ridehistory"
               className={clsx(
                 "flex items-center gap-1",
-                isActive("/ridehistory") && "text-white bg-black px-2 py-1 rounded"
+                isActive("/driver-ridehistory") && "bg-black px-2 py-1 rounded"
               )}
             >
-              <FaHistory className="me-1"/>
+              <FaHistory className="me-1" />
               Ride History
             </Link>
 
@@ -59,36 +53,34 @@ const Navbar = ({ requestedCount }) => {
               href="/feedbacks"
               className={clsx(
                 "flex items-center gap-1",
-                isActive("/ridehistory") && "text-white bg-black px-2 py-1 rounded"
+                isActive("/feedbacks") && "bg-black px-2 py-1 rounded"
               )}
             >
-              <RiFeedbackFill className="me-1"/>
-               Feedbacks
+              <RiFeedbackFill className="me-1" />
+              Feedbacks
             </Link>
-
           </>
-          
-           ) : (
+        ) : (
           <>
-              <Link
-                href="/user-dashboard"
-                className={clsx(
-                  "flex items-center gap-1", 
-                  isActive("/user-dashboard") && "text-white bg-black px-2 py-1 rounded"
-                )}
-              >
-                <FaCar className="me-1"/>
-                Ride
-              </Link>
+            <Link
+              href="/user-dashboard"
+              className={clsx(
+                "flex items-center gap-1",
+                isActive("/user-dashboard") && "bg-black px-2 py-1 rounded"
+              )}
+            >
+              <FaCar className="me-1" />
+              Ride
+            </Link>
 
             <Link
               href="/packages"
               className={clsx(
                 "flex items-center gap-1",
-                isActive("/packages") && "text-white bg-black px-2 py-1 rounded"
+                isActive("/packages") && "bg-black px-2 py-1 rounded"
               )}
             >
-              <PiPackageFill className="me-1"/>
+              <PiPackageFill className="me-1" />
               Packages
             </Link>
 
@@ -96,10 +88,10 @@ const Navbar = ({ requestedCount }) => {
               href="/ridehistory"
               className={clsx(
                 "flex items-center gap-1",
-                isActive("/ridehistory") && "text-white bg-black px-2 py-1 rounded"
+                isActive("/ridehistory") && "bg-black px-2 py-1 rounded"
               )}
             >
-              <FaHistory className="me-1"/>
+              <FaHistory className="me-1" />
               Ride History
             </Link>
 
@@ -107,10 +99,10 @@ const Navbar = ({ requestedCount }) => {
               href="/transactions"
               className={clsx(
                 "flex items-center gap-1",
-                isActive("/transactions") && "text-white bg-black px-2 py-1 rounded"
+                isActive("/transactions") && "bg-black px-2 py-1 rounded"
               )}
             >
-              <GrTransaction className="me-1"/>
+              <GrTransaction className="me-1" />
               Transactions
             </Link>
           </>
@@ -120,10 +112,10 @@ const Navbar = ({ requestedCount }) => {
           href="/profile"
           className={clsx(
             "flex items-center gap-1",
-            isActive("/profile") && "text-white bg-black px-2 py-1 rounded"
+            isActive("/profile") && "bg-black px-2 py-1 rounded"
           )}
         >
-          <FaUser/>
+          <FaUser />
           Profile
         </Link>
 
@@ -131,17 +123,16 @@ const Navbar = ({ requestedCount }) => {
           href="/contact"
           className={clsx(
             "flex items-center gap-1",
-            isActive("/contact") && "text-white bg-black px-2 py-1 rounded"
+            isActive("/contact") && "bg-black px-2 py-1 rounded"
           )}
         >
-          <FaPhone/>
+          <FaPhone />
           Contact Us
         </Link>
-
       </div>
 
       <div className="flex flex-row space-x-2">
-        <p>Welcome, {user?.fullName} </p>
+        <p>Welcome, {user?.fullName}</p>
         <UserButton />
       </div>
     </nav>
